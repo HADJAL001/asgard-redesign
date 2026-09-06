f8edfb223e4100d1548b340d8748de1866711995
HADJAL001 <HADJAL001@users.noreply.github.com>
2026-09-06 21:50:39 +0300

    Remove deprecated Sentry build options

--- next.config.mjs
+++ next.config.mjs
@@ -44,6 +44,4 @@ export default withSentryConfig(nextConfig, {
   project: process.env.SENTRY_PROJECT,
   silent: true,
   widenClientFileUpload: true,
-  disableLogger: true,
-  automaticVercelMonitors: false,
 })
